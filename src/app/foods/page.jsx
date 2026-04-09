c1db3d6c2960c422f42a9256511224a41c92e3fc
mdrakibhasan12112 <mdrakibhasan12112@gmail.com>
2026-04-10 04:22:24 +0600

    Added FoodCard

--- src/app/foods/page.jsx
+++ src/app/foods/page.jsx
@@ -1,17 +1,22 @@
+import FoodCard from '@/components/FoodCard/FoodCard';
 import React from 'react';
 
 const FoodsPage = async () => {
- 
- const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/foods');
- const data = await res.json()
- const foods = data.data 
- console.log(foods);
+  const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/foods');
+  const data = await res.json();
+  const foods = data.data;
+  console.log(foods);
 
- return (
-  <div>
-   <h1>Foods: { foods.length}</h1>
-  </div>
- );
+  return (
+    <div>
+      <h1>Foods: {foods.length}</h1>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {foods.map(food => (
+          <FoodCard key={food.id} food={food}></FoodCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default FoodsPage;
